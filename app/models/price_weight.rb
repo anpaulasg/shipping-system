f22e73060f25c4b4d05691d9c55b87c25c33c206
anpaulasg <anpaulasg@gmail.com>
2022-10-13 13:53:16 -0300

    Teste unitário preço por peso

--- app/models/price_weight.rb
+++ app/models/price_weight.rb
@@ -1,4 +1,7 @@
 class PriceWeight < ApplicationRecord
     belongs_to :delivery_mode
-    validates :min_weight, :max_weight, :weight_price, :delivery_mode, presence: true 
+    validates :min_weight, :max_weight, :weight_price, :delivery_mode, presence: true
+    validates :min_weight, :max_weight, numericality:{greater_than_or_equal_to: 0}
+    validates :weight_price, numericality: {greater_than: 0}
+    validates :weight_price, uniqueness: true
 end
--- spec/models/price_weight_spec.rb
+++ spec/models/price_weight_spec.rb
@@ -1,4 +1,86 @@
 require 'rails_helper'
 
 RSpec.describe PriceWeight, type: :model do
+    describe '#valid?' do
+        context 'presence' do
+            it 'false when max weight is empty' do
+                #Arrange
+                delivery_mode = DeliveryMode.create!(name: 'Entrega Expressa', minimum_distance: 5, maximum_distance: 800,
+                                                     minimum_weight: 2, maximum_weight: 100, initial_fee: 20)
+                price_weight = PriceWeight.create(max_weight: nil, min_weight: 10, weight_price: 0.50, delivery_mode: delivery_mode)
+                #Act
+                result = price_weight.valid? 
+    
+                #Assert
+                expect(result). to eq false 
+            end 
+    
+            it 'false when min weight is empty' do
+                #Arrange
+                delivery_mode = DeliveryMode.create!(name: 'Entrega Expressa', minimum_distance: 5, maximum_distance: 800,
+                                                    minimum_weight: 2, maximum_weight: 100, initial_fee: 20)
+                price_weight = PriceWeight.create(max_weight: 30, min_weight: nil, weight_price: 0.50, delivery_mode: delivery_mode)
+                
+                #Act
+                result = price_weight.valid? 
+    
+                #Assert
+                expect(result). to eq false
+            end
+    
+            it 'false when weight price is empty' do
+              #Arrange
+              delivery_mode = DeliveryMode.create!(name: 'Entrega Expressa', minimum_distance: 5, maximum_distance: 800,
+                                                    minimum_weight: 2, maximum_weight: 100, initial_fee: 20)
+              price_weight = PriceWeight.create(max_weight: 30, min_weight: 10, weight_price: nil, delivery_mode: delivery_mode)
+              #Act
+              result = price_weight.valid? 
+    
+              #Assert
+              expect(result). to eq false
+            end
+    
+            it 'false when delivery_mode is empty' do
+                #Arrange
+                delivery_mode = DeliveryMode.create!(name: 'Entrega Expressa', minimum_distance: 5, maximum_distance: 800,
+                                                     minimum_weight: 2, maximum_weight: 100, initial_fee: 20)
+                price_weight = PriceWeight.create(max_weight: 30, min_weight: 10, weight_price: 0.50, delivery_mode: nil)
+                #Act
+                result = price_weight.valid? 
+        
+                #Assert
+                expect(result). to eq false
+            end
+
+        end 
+    
+        context 'use_already' do 
+            it 'false when weight_price is already in use' do 
+                #Arrange
+                delivery_mode = DeliveryMode.create!(name: 'Entrega Expressa', minimum_distance: 5, maximum_distance: 800,
+                                                    minimum_weight: 2, maximum_weight: 100, initial_fee: 20)
+                first_price_weight = PriceWeight.create(max_weight: 30, min_weight: 10, weight_price: 0.50, delivery_mode: delivery_mode)
+        
+                second_price_weight = PriceWeight.create(max_weight: 50, min_weight: 10, weight_price: 0.50, delivery_mode: delivery_mode)
+                #Act
+                result = second_price_weight.valid?
+                #Assert 
+                expect(result).to eq false
+            end
+        end 
+        
+        
+        context 'parameters in general' do 
+            it 'false when less than zero' do
+                #Arrange
+                delivery_mode = DeliveryMode.create!(name: 'Entrega Expressa', minimum_distance: 5, maximum_distance: 800,
+                                                     minimum_weight: 2, maximum_weight: 100, initial_fee: 20)
+                price_weight = PriceWeight.create(max_weight: -1, min_weight: 0, weight_price: 0.50, delivery_mode: delivery_mode)
+                #Act
+                result = price_weight.valid?
+                #Assert
+                expect(result). to eq false
+            end 
+        end 
+    end 
 end
