102dfc8ee6254c7980e90bc596d24b189e8badaf
anpaulasg <anpaulasg@gmail.com>
2022-10-18 20:17:22 -0300

    Consultar Entrega

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,13 @@ Rails.application.routes.draw do
   resources :delivery_times, only: [:index, :new, :create, :edit, :update]
   
   resources :orders, only: [:index, :new, :create, :show] do 
-    get 'search', on: :collection
     resources :order_shippings, only: [:new, :create]
     resources :close_orders, only: [:new, :create]
     post 'other', on: :member
     post 'address_unknown', on: :member
     post 'absent', on: :member
     post 'lost', on: :member
+    get 'search', on: :collection
   end 
 
   resources :vehicles, only: [:index, :show, :new, :create, :edit, :update] do 
