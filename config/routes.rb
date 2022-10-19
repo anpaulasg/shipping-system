be0453b96da71323b06f63ff0ea5c8b3c70c4e5a
anpaulasg <anpaulasg@gmail.com>
2022-10-19 10:41:16 -0300

    Alterações e Melhorias

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   resources :delivery_modes, only: [:show, :new, :create, :edit, :update]
-  resources :orders, only: [:index, :show, :new, :create, :edit, :update]
   resources :price_weights, only: [:index, :new, :create, :edit, :update]
   resources :price_distances, only: [:index, :new, :create, :edit, :update]
   resources :consult_price_terms, only: [:index]
